2750baadf21378d85532018e85c66b83826b7958
Mike Eheler <mike.eheler@gmail.com>
2020-02-15 09:44:33 -0800

    Playable; needs refactor

--- SnakeGame.cs
+++ SnakeGame.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -8,7 +9,11 @@ namespace Snakexperiment
 {
     public class SnakeGame : Game
     {
-        const float MOVE_SPEED = 64.0f;
+        const double TICK_RATE = 0.125;
+        const int FIELD_HEIGHT = 38;
+        const int FIELD_WIDTH = 50;
+
+        private readonly Random _rng;
 
         private TimeSpan _lastTick;
 
@@ -16,22 +21,29 @@ namespace Snakexperiment
         private SpriteBatch _spriteBatch;
         private SpriteFont _uiFont;
 
-        private Texture2D _square;
-        private Texture2D _tile;
+        private Texture2D _appleTexture;
+        private Texture2D _snakeAliveTexture;
+        private Texture2D _snakeDeadTexture;
+        private Texture2D _tileTexture;
 
         private Vector2 _direction;
-        private Vector2 _position;
+        private Vector2 _lastPosition;
+        private Vector2 _applePosition;
+        private Queue<Vector2> _snake;
+        private int _snakeSize;
+        private bool _alive;
 
         public SnakeGame()
         {
             _graphics = new GraphicsDeviceManager(this)
             {
-                PreferredBackBufferHeight = 608,
-                PreferredBackBufferWidth = 800,
+                PreferredBackBufferHeight = FIELD_HEIGHT * 16,
+                PreferredBackBufferWidth = FIELD_WIDTH * 16,
                 PreferHalfPixelOffset = true,
                 PreferMultiSampling = true,
-                SynchronizeWithVerticalRetrace = false
+                SynchronizeWithVerticalRetrace = true
             };
+            _rng = new Random();
 
             IsFixedTimeStep = false;
             IsMouseVisible = true;
@@ -41,9 +53,14 @@ namespace Snakexperiment
 
         protected override void Initialize()
         {
+            _applePosition = new Vector2(_rng.Next(FIELD_WIDTH), _rng.Next(FIELD_HEIGHT));
+            _alive = true;
             _direction = new Vector2(1, 0);
-            _position = Vector2.Zero;
+            _snake = new Queue<Vector2>(FIELD_WIDTH * FIELD_HEIGHT);
+            _snake.Enqueue(_lastPosition);
+            _snakeSize = 10;
 
+            _lastPosition = Vector2.Zero;
             _lastTick = TimeSpan.Zero;
 
             base.Initialize();
@@ -53,24 +70,84 @@ namespace Snakexperiment
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            _square = Content.Load<Texture2D>("square");
-            _tile = Content.Load<Texture2D>("tile");
+            _snakeAliveTexture = Content.Load<Texture2D>("square");
+            _appleTexture = Content.Load<Texture2D>("apple");
+            _snakeDeadTexture = Content.Load<Texture2D>("dead");
+            _tileTexture = Content.Load<Texture2D>("tile");
             _uiFont = Content.Load<SpriteFont>("CascadiaMono");
         }
 
         protected override void Update(GameTime gameTime)
         {
+            var keyState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
-                || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                || keyState.IsKeyDown(Keys.Escape)
+                || keyState.IsKeyDown(Keys.Q))
             {
                 Exit();
             }
 
-            TimeSpan diff = gameTime.TotalGameTime - _lastTick;
-            if (diff.TotalSeconds >= 0.5)
+            if (_direction.X == 0)
             {
-                _position += _direction;
-                _lastTick = gameTime.TotalGameTime;
+                if (keyState.IsKeyDown(Keys.A) || keyState.IsKeyDown(Keys.Left))
+                {
+                    _direction.X = -1;
+                    _direction.Y = 0;
+                }
+                else if (keyState.IsKeyDown(Keys.D) || keyState.IsKeyDown(Keys.Right))
+                {
+                    _direction.X = 1;
+                    _direction.Y = 0;
+                }
+            }
+            else if (_direction.Y == 0)
+            {
+                if (keyState.IsKeyDown(Keys.W) || keyState.IsKeyDown(Keys.Up))
+                {
+                    _direction.X = 0;
+                    _direction.Y = -1;
+                }
+                else if (keyState.IsKeyDown(Keys.S) || keyState.IsKeyDown(Keys.Down))
+                {
+                    _direction.X = 0;
+                    _direction.Y = 1;
+                }
+            }
+
+            if (_alive)
+            {
+                TimeSpan diff = gameTime.TotalGameTime - _lastTick;
+                if (diff.TotalSeconds >= TICK_RATE)
+                {
+                    Vector2 newPosition = _lastPosition + _direction;
+                    if (newPosition.Y < 0
+                        || newPosition.Y >= FIELD_HEIGHT
+                        || newPosition.X < 0
+                        || newPosition.X >= FIELD_WIDTH
+                        || _snake.Contains(newPosition))
+                    {
+                        _alive = false;
+                    }
+                    else
+                    {
+                        if (newPosition == _applePosition)
+                        {
+                            _snakeSize += 5;
+                            do
+                            {
+                                _applePosition.X = _rng.Next(FIELD_WIDTH);
+                                _applePosition.Y = _rng.Next(FIELD_HEIGHT);
+                            } while (_snake.Contains(_applePosition));
+                        }
+
+                        while (_snake.Count >= _snakeSize)
+                            _snake.Dequeue();
+                        _snake.Enqueue(newPosition);
+                        _lastPosition = newPosition;
+                        _lastTick = gameTime.TotalGameTime;
+                    }
+                }
             }
 
             base.Update(gameTime);
@@ -83,19 +160,31 @@ namespace Snakexperiment
             float fps = (float)(1000.0 / gameTime.ElapsedGameTime.TotalMilliseconds);
 
             _spriteBatch.Begin();
-            for (int y = 0; y < 20; ++y) {
-                for (int x = 0; x < 25; ++x) {
-                    _spriteBatch.Draw(_tile, new Vector2(x * 32, y * 32), Color.White);
+            for (int y = 0; y < FIELD_HEIGHT / 2 + 1; ++y) {
+                for (int x = 0; x < FIELD_WIDTH / 2 + 1; ++x) {
+                    _spriteBatch.Draw(_tileTexture, new Vector2(x * _tileTexture.Width, y * _tileTexture.Height), Color.White);
                 }
             }
             _spriteBatch.End();
 
             _spriteBatch.Begin();
-            _spriteBatch.Draw(_square, _position * 16, Color.White);
+            _spriteBatch.Draw(_appleTexture, _applePosition * _appleTexture.Width, Color.White);
+            Texture2D square = _alive ? _snakeAliveTexture : _snakeDeadTexture;
+            foreach (Vector2 snakePiece in _snake)
+            {
+                _spriteBatch.Draw(square, snakePiece * square.Width, Color.White);
+            }
             _spriteBatch.End();
 
+            string uiMessage = $"size: {_snakeSize:N0}; fps: {fps:N0}";
+            var messageSize = _uiFont.MeasureString(uiMessage);
+
             _spriteBatch.Begin();
-            _spriteBatch.DrawString(_uiFont, $"fps: {fps:N2}", new Vector2(640, 576), Color.LightGray);
+            _spriteBatch.DrawString(
+                _uiFont,
+                uiMessage,
+                new Vector2(800 - messageSize.X, 608 - messageSize.Y),
+                Color.LightGray);
             _spriteBatch.End();
 
             base.Draw(gameTime);
